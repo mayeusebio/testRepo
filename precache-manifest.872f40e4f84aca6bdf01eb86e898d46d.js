5feb017e9ae8948fe41ae729368b961057e5880c
System Administrator <root@Marielles-MacBook-Pro.local>
2024-02-16 16:32:23 -0800

    Updates

---
+++ precache-manifest.872f40e4f84aca6bdf01eb86e898d46d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b4894563b00c6d691ab227c65637a4",
+    "url": "/testRepo/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/testRepo/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/testRepo/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/testRepo/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/testRepo/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "105383fe97c584c0252b",
+    "url": "/testRepo/static/js/runtime~main.45e5ad0c.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/testRepo/static/media/logo.06e73328.svg"
+  }
+]);
